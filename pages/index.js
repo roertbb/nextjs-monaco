bc9c4d6f0473d8b98054fcb1176d80ca9c6a31f7
roertbb <robert.banaszak1997@gmail.com>
2021-12-08 21:17:23 +0100

    add sandpack

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,25 @@
 import React from "react";
 import Editor from "@monaco-editor/react";
+import { SandpackPreview, SandpackProvider } from "@codesandbox/sandpack-react";
+
+const p5Template = `
+import p5 from 'p5';
+
+const sketch = (s) => {
+    s.setup = () => {
+        s.createCanvas(500, 500);
+    }
+
+    s.draw = () => {
+        s.background(0);
+        s.circle(20,20,20);
+    }
+}
+
+const sketchInstance = new p5(sketch);`;
 
 export default function Home() {
-  const [editorValue, setEditorValue] = React.useState('const test = "test"');
+  const [editorValue, setEditorValue] = React.useState(p5Template);
   const editorRef = React.useRef();
 
   const handleEditorDidMount = (value, editor) => {
@@ -23,7 +40,7 @@ export default function Home() {
   };
 
   return (
-    <div>
+    <div style={{ display: "flex" }}>
       <Editor
         width="40vw"
         height="40vh"
@@ -33,6 +50,20 @@ export default function Home() {
         language="javascript"
         theme="vs-dark"
       />
+      <SandpackProvider
+        customSetup={{
+          entry: "/index.js",
+          dependencies: { p5: "latest" },
+          files: {
+            "/index.js": {
+              code: editorValue,
+              active: true,
+            },
+          },
+        }}
+      >
+        <SandpackPreview />
+      </SandpackProvider>
     </div>
   );
 }
